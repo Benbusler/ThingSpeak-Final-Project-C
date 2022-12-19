15024951bdfc1964cfc44d176e3f6a0195574afe
Benbusler <35205599+Benbusler@users.noreply.github.com>
2022-12-19 16:09:44 -0500

    Lol finished project i think

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -17,7 +17,9 @@ namespace WindowsFormsApp2
         int startflag = 0;
         int flag_sensor;
         string RxString;
-        string temp, voice = "0";
+        string temp = "0";
+        string light = "0";
+        string humidity = "0";
         char charb = 'B';
         public Form1()
         {
@@ -25,8 +27,8 @@ namespace WindowsFormsApp2
         }
 
         private void Serial_start_Click(object sender, EventArgs e)
-        {
-            serialPort1.PortName = "COM4";
+        {   
+            serialPort1.PortName = "COM7";
             serialPort1.BaudRate = 115200;
             serialPort1.Open();
             if (serialPort1.IsOpen)
@@ -49,7 +51,8 @@ namespace WindowsFormsApp2
 
             label1.Text = client.DownloadString("http://api.thingspeak.com/channels/1563508/field/field1/last.text");//use your channel id
 
-            //label1.Text = client.DownloadString("http://api.thingspeak.com/channels/1563508/field/field1/last.text");//use your channel id
+            
+
 
         }
 
@@ -60,7 +63,25 @@ namespace WindowsFormsApp2
 
         private void Current_Data_Click(object sender, EventArgs e) //Recieving data from the text box
         {
-            textBox1.AppendText(RxString);
+            textBox1.ResetText();
+            temp = temp;
+            textBox1.AppendText(temp);
+
+            textBox2.ResetText();
+            light = light;
+            textBox2.AppendText(light);
+
+
+            textBox3.ResetText();
+            textBox3.AppendText(humidity);
+
+
+            textBox4.ResetText();
+            textBox4.AppendText("" + RxString.IndexOf('B'));
+            textBox5.ResetText();
+            textBox5.AppendText("" + (RxString.IndexOf('B') + 2));
+
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -72,7 +93,7 @@ namespace WindowsFormsApp2
         {
             if (serialPort1.IsOpen)
                 serialPort1.Close();
-            serialPort1.PortName = "COM4";
+            serialPort1.PortName = "COM7";
             serialPort1.BaudRate = 115200;
         }
 
@@ -83,30 +104,24 @@ namespace WindowsFormsApp2
                 if (serialPort1.IsOpen) serialPort1.Close();
                 try
                 {
-                    if (RxString[0] == 'B')
+                    if (1 == 1)
                     {
-                        flag_sensor = 10;
+                        flag_sensor = 11;
                     }
                     const string WRITEKEY = "V0WUME6TSZGTVJWV"; ////use your channel API keys
                     string strUpdateBase = "http://api.thingspeak.com/update";
                     string strUpdateURI = strUpdateBase + "?key=" + WRITEKEY;
-                    string strField1 = textBox1.Text;
-                    //string strField1 = temp;
-                    //string strField2 = voice;
-                    //string strField2 = "42";
+                    string strField1 = RxString;
+                    
                     HttpWebRequest ThingsSpeakReq;
                     HttpWebResponse ThingsSpeakResp;
 
                     if (flag_sensor == 11)
                     {
-                        strUpdateURI += "&field1=" + strField1;
-                        //strUpdateURI += "&field1=" + temp;
+                        strUpdateURI += "&field1=" + temp + "&field2=" + light + "&field3=" + humidity;
+                        //strUpdateURI += "&field1=" + strField1 + "&field2=" + strField1 + "&field3=" + strField1;
                     }
-                    /* else if (flag_sensor == 12)
-                     {
-                       //  strUpdateURI += "&field2=" + voice;
-                          strUpdateURI += "&field2=" + strField1;
-                     }*/
+                   
                     else
                     {
                     }
@@ -123,41 +138,62 @@ namespace WindowsFormsApp2
                 catch (Exception ex)
                 {
                 }
-                textBox1.Text = "";
+              //textBox1.Text = "";
                 serialPort1.Open();
             }
         }
 
-       
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
 
-        private void SerialPort1_DataReceived(object sender,
-            System.IO.Ports.SerialDataReceivedEventArgs e)
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SerialPort1_DataReceived(object sender,System.IO.Ports.SerialDataReceivedEventArgs e)
         {
             Console.WriteLine("Data Received");
             RxString = serialPort1.ReadExisting();
-            if (RxString.Contains(charb))
+            int BPlace = RxString.LastIndexOf('B');
+            RxString = RxString.Substring(0,RxString.LastIndexOf('B')-1);
+            
+            //B10aB10bB10c
+            if(RxString.IndexOf('a') - 2 > 0)
             {
-                startflag = 9;
+                temp = RxString.Substring(RxString.IndexOf('a') - 2, 2);
             }
-            else { }
-            if (startflag == 9)
+            if (RxString.IndexOf('b') - 3 > 0)
             {
-                startflag++;
+                light = RxString.Substring(RxString.IndexOf('b') - 2, 2);
             }
-            else if (startflag == 10)
-            {
-                temp = RxString;
-                startflag++;
-            }
-            else if (startflag == 11)
-            {
-                voice = RxString;
-                startflag++;
-            }
-            else
-            {
-                // startflag = 0;
-            }
+            
+            humidity = "50";
+
+            
+
             this.Invoke(new EventHandler(Current_Data_Click));
         }
     }
